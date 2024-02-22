f4b2dba629405115732e55b77b18a5b702de6874
EslamKamel89 <eslam_kamel_89@hotmail.com>
2024-02-23 01:20:43 +0200

    discount

--- items/items.php
+++ items/items.php
@@ -5,7 +5,8 @@ $userId = filterRequest('userId');
 // $stmt = $con->prepare("SELECT * FROM `itemsview` WHERE `items_categories_id` = ?");
 $stmt = $con->prepare("SELECT
 itemsview.*,
-1 AS favorite_item
+1 AS favorite_item , 
+(`items_price` -(`items_price` * `items_discount`/100)) as items_price_discount
 FROM
 itemsview
 INNER JOIN favorite ON favorite.favorite_itemid = itemsview.items_id AND favorite.favorite_userid = 22 
@@ -13,7 +14,8 @@ AND itemsview.items_categories_id = $categoryId
 UNION ALL
 SELECT
 itemsview.*,
-0 AS favorite_item
+0 AS favorite_item,
+(`items_price` -(`items_price` * `items_discount`/100)) as items_price_discount
 FROM
 itemsview
 WHERE
